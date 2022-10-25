047ea9a2926e95758e33b38a0a722f0b89f71daa
vilcinskij <vilcinskij@gmail.com>
2022-10-25 09:46:49 +0300

    moviesTask

--- 2022-10-24-json/movies/movies.js
+++ 2022-10-24-json/movies/movies.js
@@ -2,20 +2,39 @@
 let moviesContainer = document.querySelector('#movies-container')
 
 
-let moviesList = document.createElement('ul');
-
-
-
-moviesContainer.append(moviesList)
-
 
 fetch('./movies.json')
-    .then(response => response.json())
-    .then(movies => 
-        movies.map(interest => {
-            let interestsListItem = document.createElement('li');
-            interestsListItem.textContent = interest.value;
-            moviesList.append(interestsListItem)
-        
-        }));
+    .then(res => res.json())
+    .then(movies => {
+        movies.map(movie => {
+            // addListItem(movie)
+            let moviesListItem = document.createElement('div');
+            moviesListItem.classList.add('card', 'border-dark', 'p-3', 'mb-4')
+            moviesListItem.style.width = '360px'
 
+            let title = document.createElement('h4');
+            title.textContent = `${movie.title}, ${movie.year}`;
+            title.classList.add('card-title');
+
+            let description = document.createElement('p');
+            description.textContent = movie.description;
+
+            let genres = addList(movie.genres);
+            let directors = addList(movie.directors); 
+            let cast = addList(movie.cast); 
+            
+
+            moviesContainer.append(moviesListItem)
+            moviesListItem.append(title, genres, description, directors, cast);
+        })
+    })
+
+function addList(elementsArr) {
+    let list = document.createElement('ul');
+    elementsArr.map(element => {
+        let listElement = document.createElement('li');
+        listElement.textContent = element;
+        list.append(listElement);
+    })
+    return list
+}
