6163e61124ceec4fe6ab2fc7f0a533498d5f1dcf
vilcinskij <vilcinskij@gmail.com>
2022-11-10 13:28:25 +0200

    oop-task

---
+++ 2022-11-09-oop/oop.js
@@ -0,0 +1,88 @@
+// GYVŪNAI
+
+// BASE class
+// superclass Mammal klasei
+class Animal {
+    constructor(legs, color, gender, warmBlooded) {
+      this.legs = legs;
+      this.color = color;
+      this.gender = gender;
+      this.warmBlooded = warmBlooded;
+    }
+  
+    eat() {
+      return "I'm eating.";
+    }
+  
+    sleep() {
+      return "I'm sleeping.";
+    }
+  
+    renderText(text, selector) {
+      if (!text || !selector) return;
+  
+      const element = document.querySelector(selector);
+      element.textContent = text;
+    }
+  }
+  
+  // ŽINDUOLIAI
+  // subclass Animal klasei
+  // superclass Dog klasei
+  class Mammal extends Animal {
+    constructor(legs, color, gender, warmBlooded, herbivore) {
+      super();
+      this.legs = legs;
+      this.color = color;
+      this.gender = gender;
+      this.herbivore = herbivore;
+      this.warmBlooded = warmBlooded;
+    }
+  
+    milkFeeding() {
+      return 'Feeding...';
+    }
+  
+    getInfo() {
+      return 'test';
+    }
+  }
+  
+  // RŪŠIS
+  // subclass Mammal klasei
+  class Dog extends Mammal {
+    constructor(color, gender, name, barking = true, legs = 4) {
+  
+      super(legs, color, gender);
+      this.barking = barking;
+      this.name = name;
+      this.warmBlooded = true;
+      this.herbivore = false
+    }
+  
+    bark() {
+      if (this.barking) {
+        return "I'm barking.";
+      } else {
+        return "I can't bark.";
+      }
+    }
+  
+    getInfo() {
+      return `Color is ${this.color} and have ${this.legs} legs...`;
+    }
+  }
+  
+  const dog1 = new Dog('black', 'male', 'Brisius');
+  const dog2 = new Dog('brown', 'female', 'Kitas vardas');
+  
+  console.log(dog1.milkFeeding());
+  console.log(dog1.eat());
+  console.log(dog2.sleep());
+  console.log(dog1.bark());
+  console.log(dog2.bark());
+  
+  const outputText = dog2.getInfo();
+  dog2.renderText(outputText, 'h1');
+  
+  console.log(dog1);
--- 2022-11-09-oop/script.js
+++ 2022-11-09-oop/script.js
@@ -7,41 +7,71 @@ const keturkampis1 = {
     },
     perimetras() {
         return (this.aukstis + this.plotis) * 2
-    }
+    },
+    parodytiDuomenis(selector) {
+        const element = document.querySelector(selector);
+        element.textContent = `Keturkampio plotis: ${this.plotis}, keturkampio ilgis: ${this.aukstis}, keturkampio perimetras: ${this.perimetras()}, keturkampio plotas: ${this.plotas()}`
+    },
 }
 
 const keturkampis2 = {
-    aukstis: 100,
+    aukstis: 200,
     plotis: 100,
+    spalva: 'raudona',
     plotas() {
         return this.aukstis * this.plotis
     },
     perimetras() {
         return (this.aukstis + this.plotis) * 2
-    }
+    },
+    pusePloto() {
+        return this.plotas() / 2
+    },
+    parodytiDuomenis(selector) {
+        const element = document.querySelector(selector);
+        element.textContent = `Keturkampio plotis: ${this.plotis}, keturkampio ilgis: ${this.aukstis}, keturkampio perimetras: ${this.perimetras()}, keturkampio plotas: ${this.plotas()}`
+    },
 }
 
 const keturkampis3 = {
-    aukstis: 100,
-    plotis: 100,
+    aukstis: 150,
+    plotis: 80,
     plotas() {
         return this.aukstis * this.plotis
     },
     perimetras() {
         return (this.aukstis + this.plotis) * 2
-    }
+    },
+    informacija(){
+        return `Keturkampio plotis: ${this.plotis}, keturkampio aukštis: ${this.aukstis}`
+    },
+    parodytiDuomenis(selector) {
+        const element = document.querySelector(selector);
+        element.textContent = `Keturkampio plotis: ${this.plotis}, keturkampio ilgis: ${this.aukstis}, keturkampio perimetras: ${this.perimetras()}, keturkampio plotas: ${this.plotas()}`
+    },
 }
 
+// console.log(keturkampis1.plotas());
+// console.log(keturkampis1.perimetras());
 
-console.log(keturkampis1.plotas());
-console.log(keturkampis1.perimetras());
+// console.log(keturkampis2.plotas());
+// console.log(keturkampis2.perimetras());
+// console.log(keturkampis2.spalva);
+// console.log(keturkampis2.pusePloto());
 
+// console.log(keturkampis3.plotas());
+// console.log(keturkampis3.perimetras());
+console.log(keturkampis3.informacija());
+
+keturkampis1.parodytiDuomenis('#keturkampis1')
+keturkampis2.parodytiDuomenis('#keturkampis2')
+keturkampis3.parodytiDuomenis('#keturkampis3')
 
 
 class Rectangle {
-    constructor(rectHeight, recWidth) {
+    constructor(rectHeight, rectWidth) {
         this.height = rectHeight
-        this.width = recWidth
+        this.width = rectWidth
     }
     area() {
         return this.height * this.width
@@ -51,7 +81,7 @@ class Rectangle {
 const rect1 = new Rectangle(150, 550);
 const rect2 = new Rectangle(50, 5);
 
-console.log(rect1);
-console.log(rect2.area());
+// console.log(rect1);
+// console.log(rect2.area());
 
 
