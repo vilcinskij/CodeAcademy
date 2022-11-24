834bd7f31112d09dd3d902007109e202bba32981
vilcinskij <vilcinskij@gmail.com>
2022-11-24 02:15:13 +0200

    cars task

--- 2022-11-16-react/react-learning/src/components/CarItem/CarItem.js
+++ 2022-11-16-react/react-learning/src/components/CarItem/CarItem.js
@@ -3,18 +3,23 @@ import './CarItem.css'
 
 const CarItem = ({ data }) => {
     let { brand, model, engine, basePrice, millage, color, image } = data
+    let carTitle = model ? `${brand} (${model})` : brand
     return (
         <div className='car-item'>
-            <h2 className='car-title'>{brand} ({model})</h2>
+            <h2 className='car-title'>{carTitle}</h2>
             <div className='car-img'>
-                <img src={image} alt="car model brand"/>
+                <img src={image} alt={`car ${brand} ${model} ${color}"`} />
             </div>
-            <ul className='car-info-list'>
-            <li></li>
-            <li></li>
-            <li></li>
-            </ul>
-        </div>
+
+            {(engine || basePrice || millage || color) && (
+                <ul className='car-info-list'>
+                    {engine && <li><strong>Engine: </strong>{engine}</li>}
+                    {color && <li><strong>Color: </strong>{color}</li>}
+                    {basePrice && <li><strong>Base price: </strong>{basePrice} €</li>}
+                    {millage && <li><strong>Millage: </strong>{millage} km</li>}
+                </ul>
+            )}
+        </div >
     )
 }
 
