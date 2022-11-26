c8fb935174b521c82dbf47f82547b99d977c518f
vilcinskij <vilcinskij@gmail.com>
2022-11-26 19:33:46 +0200

    CounterPage

--- 2022-11-16-react/react-learning/src/pages/CounterPage/CounterPage.js
+++ 2022-11-16-react/react-learning/src/pages/CounterPage/CounterPage.js
@@ -5,30 +5,38 @@ const CounterPage = () => {
 
     const countDefault = 5;
     const [count, setCount] = useState(countDefault);
-    const countColor = count < 5 ? 'red' : 'green';
+    const [grades, setGrades] = useState([])
 
-    function counterHandler(num) {
-        setCount(prevState => prevState + num)
-    }
+    const countClass = num => num < 5 ? 'red' : 'green';
 
-    function counterInputHandler(event) {
-        setCount(event.target.value)
+    const counterHandler = num => setCount(prevState => prevState + num)
+    const counterInputHandler = event => setCount(Number(event.target.value))
+    const addGradeHandler = () => {
+        setGrades(prevState => [...prevState, count])
+        setCount(countDefault)
     }
 
     return (
         <div id="numbers">
-            <input type="number" value={count} onChange={counterInputHandler} />
-            <h3 className={countColor}>{count}</h3>
+            <input type="number" value={count} min={1} max={10} onChange={counterInputHandler} />
+            <h3 className={countClass(count)}>{count}</h3>
             <div>
                 <button onClick={() => counterHandler(-2)} disabled={count < 3 ? true : null}>-2</button>
                 <button onClick={() => counterHandler(-1)} disabled={count < 2 ? true : null}>-</button>
                 <button onClick={() => counterHandler(1)} disabled={count > 9 ? true : null} >+</button>
                 <button onClick={() => counterHandler(2)} disabled={count > 8 ? true : null}>+2</button>
-                <button onClick={() => setCount(countDefault)}>RESET</button>
-                <button >Įrašyti balą</button>
-                <button>Pašalinti įrašus</button>
+                <button onClick={() => setCount(countDefault)}>Reset</button>
+                <button onClick={addGradeHandler} >Add grade</button>
+                <button>Delete all grades</button>
             </div>
-        </div>
+            {grades.length > 0 &&
+                < ul className='grades-list'>
+                    {grades.map((grade, index) => {
+                        return <li className={countClass(grade)} key={index}>{grade}</li>
+                    })}
+                </ul>
+            }
+        </div >
     )
 }
 
