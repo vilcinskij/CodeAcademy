2e3d087f31efd9f193cd012c64ea50ebc6bdef60
vilcinskij <vilcinskij@gmail.com>
2022-11-26 02:26:45 +0200

    counterHandler

--- 2022-11-16-react/react-learning/src/pages/CounterPage/CounterPage.js
+++ 2022-11-16-react/react-learning/src/pages/CounterPage/CounterPage.js
@@ -1,23 +1,30 @@
+import './CounterPage.css'
 import React, { useState } from 'react'
-
 
 const CounterPage = () => {
 
-    const [num, setNum] = useState(6)
+    const countDefault = 5;
+    const [count, setCount] = useState(countDefault);
+    const countColor = count < 5 ? 'red' : 'green'
 
-    function numHandle() {
-        setNum(num - 2)
+    function resetHandle() {
+        setCount(countDefault)
+    };
+
+    function counterHandler(num) {
+        setCount(prevState => prevState + num)
     }
 
     return (
         <div id="numbers">
-            <h3>{num}</h3>
-            <button onClick={numHandle}>-2</button>
-            <button>-</button>
-            <button>+</button>
-            <button>+2</button>
-            <button>RESET</button>
-            <button>Įrašyti balą</button><button>Pašalinti įrašus</button>
+            <h3 className={countColor}>{count}</h3>
+            <button onClick={() => counterHandler(-2)} disabled={count < 3 && true}>-2</button>
+            <button onClick={() => counterHandler(-1)} disabled={count < 2 && true}>-</button>
+            <button onClick={() => counterHandler(1)} disabled={count > 9 && true} >+</button>
+            <button onClick={() => counterHandler(2)} disabled={count > 8 && true}>+2</button>
+            <button onClick={resetHandle}>RESET</button>
+            <button >Įrašyti balą</button>
+            <button>Pašalinti įrašus</button>
         </div>
     )
 }
