9e812521dee723f696aaf36c161a1a90968859be
vilcinskij <vilcinskij@gmail.com>
2022-11-26 04:18:07 +0200

    input

--- 2022-11-16-react/react-learning/src/pages/CounterPage/CounterPage.js
+++ 2022-11-16-react/react-learning/src/pages/CounterPage/CounterPage.js
@@ -5,26 +5,29 @@ const CounterPage = () => {
 
     const countDefault = 5;
     const [count, setCount] = useState(countDefault);
-    const countColor = count < 5 ? 'red' : 'green'
-
-    function resetHandle() {
-        setCount(countDefault)
-    };
+    const countColor = count < 5 ? 'red' : 'green';
 
     function counterHandler(num) {
         setCount(prevState => prevState + num)
     }
 
+    function counterInputHandler(event) {
+        setCount(event.target.value)
+    }
+
     return (
         <div id="numbers">
+            <input type="number" value={count} onChange={counterInputHandler} />
             <h3 className={countColor}>{count}</h3>
-            <button onClick={() => counterHandler(-2)} disabled={count < 3 && true}>-2</button>
-            <button onClick={() => counterHandler(-1)} disabled={count < 2 && true}>-</button>
-            <button onClick={() => counterHandler(1)} disabled={count > 9 && true} >+</button>
-            <button onClick={() => counterHandler(2)} disabled={count > 8 && true}>+2</button>
-            <button onClick={resetHandle}>RESET</button>
-            <button >Įrašyti balą</button>
-            <button>Pašalinti įrašus</button>
+            <div>
+                <button onClick={() => counterHandler(-2)} disabled={count < 3 ? true : null}>-2</button>
+                <button onClick={() => counterHandler(-1)} disabled={count < 2 ? true : null}>-</button>
+                <button onClick={() => counterHandler(1)} disabled={count > 9 ? true : null} >+</button>
+                <button onClick={() => counterHandler(2)} disabled={count > 8 ? true : null}>+2</button>
+                <button onClick={() => setCount(countDefault)}>RESET</button>
+                <button >Įrašyti balą</button>
+                <button>Pašalinti įrašus</button>
+            </div>
         </div>
     )
 }
